58a0221458d8367f3e55d9295ee98888f0145fa4
samanthacel <samantha.michelle1711@gmail.com>
2024-11-30 14:17:45 +0700

    :rotating_light: navbar ongoing fragments

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,6 +25,10 @@ android {
             )
         }
     }
+
+    buildFeatures{
+        viewBinding = true
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -40,4 +44,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
 }
--- app/src/main/java/com/example/planescaper/MainActivity.java
+++ app/src/main/java/com/example/planescaper/MainActivity.java
@@ -10,23 +10,55 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.example.planescaper.databinding.ActivityMainBinding;
+import com.example.planescaper.fragments.OrdersFragment;
+import com.example.planescaper.fragments.PlanFragment;
+import com.example.planescaper.fragments.SavedFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+//        binding = ActivityMainBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
+//        replaceFragment(new PlanFragment());
+
+        EdgeToEdge.enable(this);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+//        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+//            switch (item.getItemId()){
+//                case R.id.menuSaved:
+//                    replaceFragment(new SavedFragment());
+//                    break;
+//                case R.id.menuPlan:
+//                    replaceFragment(new PlanFragment());
+//                    break;
+//                case R.id.menuOrders:
+//                    replaceFragment(new OrdersFragment());
+//                    break;
+//            }
+//            return true;
+//        });
+    }
 
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.menubarFrameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
